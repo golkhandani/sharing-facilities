3a526e1baeee55f9c16e998b49544feb49193a86
MohammadRezaRahimianGolkhandani <site.golkhandani@outlook.com>
2021-07-03 15:17:19 +0430

    Fix: db uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { Facility, Reservation, Room } from './models/room';
 
 @Module({
   imports: [
-    MongoModule.forRoot('mongodb://localhost:27017', 'SharingFacility'),
+    MongoModule.forRoot('mongodb+srv://m001-student:m001-mongodb-basics@cluster0.k5tdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 'SharingFacility'),
     MongoModule.forFeature([Facility.name, Room.name, Reservation.name])
   ],
   controllers: [AppController],
